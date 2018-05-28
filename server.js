0dc0660916d0e3d9d20e0afb36a1afa500973a3c
Michael-PC\Michael <michaelrtinelli@gmail.com>
2018-05-28 15:27:57 -0300

    deploytoheroku8

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-const forceSSL = function() {
-    return function(req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-          ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  };
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/testeapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/testeapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/testeapp/index.html'))
 });
 
 // Start the app by listening on the default Heroku port
